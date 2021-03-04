57f854970cf976c6b20f2d2794e71dd8d59b2c5c
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-03-04 01:18:01 -0700

    Refactored code

--- blackjack.rb
+++ blackjack.rb
@@ -1,73 +1,112 @@
-
-
 require_relative 'deck'
 
-# Create methods for program
-def initial_card_deal
-  2.times {
-    @deck.draw_card
-    @deck.draw_card_dealer
-  }
-end
-
-def card_hit
-  response = 'yes'
-  until response == 'no'
-    puts "Would you like to draw another card? (yes/no)"
-    response = gets.chomp.to_s
-    puts "\n"
-    if response == 'yes'
+class Blackjack
+  # Create methods for program
+  def initial_card_deal
+    2.times {
       @deck.draw_card
-      @deck.display_my_hand
-    else
-      break
+      @deck.draw_card_dealer
+    }
+  end
+
+  def card_hit
+    response = 'yes'
+    until response == 'no'
+      puts "Would you like to draw another card? (yes/no)"
+      response = gets.chomp.to_s
+      puts "\n"
+      if response == 'yes'
+        @deck.draw_card
+        @deck.display_my_hand
+      else
+        break
+      end
     end
   end
-end
 
-def who_won
-  if @deck.show_card_values_dealer > 21
-    puts "You Won!"
-  elsif @deck.show_card_values == @deck.show_card_values_dealer
-    puts "You tied!"
-  elsif @deck.show_card_values > @deck.show_card_values_dealer
-    puts "You Won!"
-  elsif @deck.show_card_values < @deck.show_card_values_dealer
-    puts "You Lost!"
+  def who_won
+    dealer_value = @deck.show_card_values_dealer
+    puts "Your total amount of points is: #{@player_value}\n"
+    puts "The dealer's total amount of points is #{dealer_value}\n"
+    if dealer_value > 21
+      puts "You Won!"
+      $wallet += (@bet_amount * 2)
+    elsif @player_value == dealer_value
+      puts "You tied!"
+      $wallet += (@bet_amount)
+    elsif @player_value > dealer_value
+      puts "You Won!"
+      $wallet += (@bet_amount * 2)
+    elsif @player_value < dealer_value
+      puts "You Lost!"
+    end
   end
-end
 
-def blackjack_game
-  #Begin Program
-  puts "Welcome to 21! We hope you enjoy!\n\n"
-  puts "How many dollars would you like to bet?"
-  bet_amount = gets.chomp.to_i
-  puts "\n"
+  def bet_amount_choice
+    if $wallet >= @bet_amount
+      $wallet -= @bet_amount
+    else
+      puts "You do not have enough funds to bet $#{@bet_amount}."
+      puts "Would you like to 1. Choose a different amount or 2. Go to main menu?"
+      bet_choice = gets.chomp.to_i
+      if bet_choice == 1
+        blackjack_game
+      else
+        @app.display_main_menu
+      end
+    end 
+  end
 
-  # Create deck and shuffle the cards
-  @deck = Deck.new
-  @deck.shuffle_cards
+  def blackjack_game
+    #Begin Program
+    @app = App.new
+    puts "Welcome to 21! We hope you enjoy!\n\n"
+    puts "How many dollars would you like to bet?"
+    @bet_amount = gets.chomp.to_i
+    puts "\n"
+    bet_amount_choice
 
-  # Deal 2 cards to the player and the dealer. Display player's hand.
-  initial_card_deal
-  @deck.display_my_hand
+    # Create deck and shuffle the cards
+    @deck = Deck.new
+    @deck.shuffle_cards
 
-  # Show dealers first card
-  @deck.show_dealer_1st_card
+    # Deal 2 cards to the player and the dealer. Display player's hand.
+    initial_card_deal
+    @deck.display_my_hand
 
-  # Ask the player if they'd like to be hit with another card until they say no
-  card_hit
+    # Show dealers first card
+    @deck.show_dealer_1st_card
 
-  # Calcuate the value of the players hand
-  if @deck.show_card_values > 21
-    puts "You Lost!"
+    # Ask the player if they'd like to be hit with another card until they say no
+    card_hit
+
+    # Calcuate the value of the players hand
+    @player_value = @deck.show_card_values
+    if @player_value > 21
+      puts "Your total amount of points is: #{@player_value}\n"
+      puts "You Busted!"
+      puts "\nThank you for playing 21!\n\n"
+      puts "Would you like to play again? (1) Yes (2) No" 
+      blackjack_choice = gets.chomp.to_i
+      if blackjack_choice == 1
+        blackjack_game
+      else
+        @app.display_main_menu
+      end
+    end
+
+
+    # Calculate the value of the players hand and say who won
+    who_won
     puts "\nThank you for playing 21!"
-    exit
+
+    puts "Would you like to play again? (1) Yes (2) No" 
+      blackjack_choice = gets.chomp.to_i
+      if blackjack_choice == 1
+        blackjack_game
+      else
+        @app.display_main_menu
+      end
+    
   end
-
-  # Calculate the value of the players hand and say who won
-  who_won
-
-  puts "\nThank you for playing 21!"
 end
-
--- roulette.rb
+++ roulette.rb
@@ -1,93 +1,104 @@
 class Roulette
-@balance = 100
-puts "welcome to the Roulette table. your balance is #{@balance}"
-puts "your balance is #{@balance}"
-puts "how much would you like to bet?"
-@bet = gets.chomp.to_i
-@menu =[
-    {key:1, name: "color"},
-    {key:2, name: "odd or even"},
-    {key:3, name: "number"},
-    {key:4, name: "view balance?"},
-    {key:5, name: "Back to Main Menu"}
-]
-@wallet =[ 
-    @balance += @bet
-]
 
-def display_options
-    @menu.each_with_index do | menu, index |
-        puts " #{menu[:key]}, #{menu[:name]}"
-    end
-end
-def table_number
-    @results = rand(1..36)
-end
-def table_color
-    red = 1
-    black = 2
-    @color_results = rand(1..2)
-end
-def table_odd_or_even
-    odd = 1
-    even = 2
-    @odds_results = rand(1..2)
-end
+    def start_game
 
-def display_menu
-    puts "please choose how you would like to bet"
-    display_options
-    choice = gets.chomp.to_i
-    case choice
-    when 1 
-        puts " would you like to bet on 1. red or 2. black?"
-        color_choice = gets.chomp.to_i
-        table_color
-        if @color_results == color_choice
-            puts "YOU WIN!"
-            @bet *= 2
-            @balance += @bet
-            display_menu
-        else 
-            puts "YOU LOSE.. TRY AGAIN!"
-            @balance -= @bet
-            display_menu
-        end
-    when 2 
-        puts "would you like to bet on 1. odds or 2. even?"
-        odds_choice = gets.chomp.to_i
-        @table_odd_or_even
-        if @odds_results == odds_choice
-            puts "GREAT JOB!"
-            @bet *= 2
-            @balance += @bet
-            display_menu
-        else
-            puts "YOU LOSE.. TRY AGAIN!"
-            @balance -= @bet
-            display_menu
-        end
-    when 3
-        puts "what number 1-36 would you like to put your money on?"
-        number_choice = gets.chomp.to_i
-        table_number
-        if @results == number_choice
-            puts "ITS YOUR LUCKY DAY!"
-            @bet *= 10
-            @balance += @bet
-            display_menu
-        else 
-            puts "NO LUCK BOB.. TRY AGAIN!"
-            @balance -= @bet
-            display_menu
-        end
-    when 4 
-        puts "your balance is #{@balance}"
-        display_menu
-    else
-        puts "your balance was #{@balance}"
     end
-    #add way to get back to main menu
-  
-end
+
+  def display_options_roulette
+      @menu_roulette =[
+      {key:1, name: "Color"},
+      {key:2, name: "Odd or even"},
+      {key:3, name: "Number"},
+      {key:4, name: "View balance?"},
+      {key:5, name: "Exit"}
+  ]
+
+      @menu_roulette.each_with_index do | menu, index |
+          puts "#{menu[:key]}, #{menu[:name]}"
+      end
+  end
+
+  def table_number
+      @results = rand(1..36)
+  end
+
+  def table_color
+      red = 1
+      black = 2
+      @color_results = rand(1..2)
+  end
+
+  def table_odd_or_even
+      odd = 1
+      even = 2
+      @odds_results = rand(1..2)
+  end
+
+  def display_menu2
+      @app = App.new
+      puts "welcome to the Roulette table. your balance is $#{$wallet}"
+      puts "Please choose how you would like to bet"
+      display_options_roulette
+      choice = gets.chomp.to_i
+      case choice
+      when 1 
+        puts "How much would you like to bet?"
+        @bet = gets.chomp.to_i
+          puts "Would you like to bet on 1. Red or 2. Black?"
+          color_choice = gets.chomp.to_i
+          table_color
+          if @color_results == color_choice
+              puts "YOU WIN!"
+              @bet *= 2
+              $wallet += @bet
+              display_menu2
+          else 
+              puts "YOU LOSE.. TRY AGAIN!"
+              $wallet -= @bet
+              display_menu2
+          end
+      when 2 
+        puts "How much would you like to bet?"
+        @bet = gets.chomp.to_i
+          puts "Would you like to bet on 1. Odds or 2. Even?"
+          odds_choice = gets.chomp.to_i
+          @table_odd_or_even
+          if @odds_results == odds_choice
+              puts "GREAT JOB!"
+              @bet *= 2
+              $wallet += @bet
+              display_menu2
+          else
+              puts "YOU LOSE.. TRY AGAIN!"
+              $wallet -= @bet
+              display_menu2
+          end
+      when 3
+        puts "How much would you like to bet?"
+        @bet = gets.chomp.to_i
+          puts "What number 1-36 would you like to put your money on?"
+          number_choice = gets.chomp.to_i
+          table_number
+          if @results == number_choice
+              puts "ITS YOUR LUCKY DAY!"
+              @bet *= 10
+              $wallet += @bet
+              display_menu2
+          else 
+              puts "NO LUCK BOB.. TRY AGAIN!"
+              $wallet -= @bet
+              display_menu2
+          end
+      when 4 
+          puts "Your balance is $#{$wallet}"
+          display_menu2
+      when 5
+          puts "THANK YOU COME AGAIN"
+          @app.display_main_menu
+      else
+          puts "INVALID CHOICE"
+          display_menu2
+      end
+  end
+
 end
